e4e4639ec7def226fd87cfa2b9cfe30c18da6bbc
MAX SANKRIS <MAX SANKRIS@Jorge.Rodriguez>
2017-08-16 18:58:52 -0600

    practica1_201314013_v1

---
+++ src/buscaminas_201314073/Buscaminas_201314073.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas_201314073;
+
+/**
+ *
+ * @author MAX SANKRIS
+ */
+public class Buscaminas_201314073 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Nombre:Jorge Rodriguez");
+        System.out.println("carné:201314073");
+        System.out.println("IPC A PRACTICA 1");
+        System.out.println("Buscaminas");
+        
+         Menu_201314073 menu=new Menu_201314073();
+         menu.Menuprincipal();
+        
+        
+        
+        
+        
+        
+    }
+    
+}
